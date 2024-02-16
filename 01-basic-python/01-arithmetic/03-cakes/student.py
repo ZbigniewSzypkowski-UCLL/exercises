28a14edbeb384ee7b387eebde1010cb6908ff077
ZbigniewSzypkowski-UCLL <zbigniew.szypkowski@student.ucll.be>
2024-02-16 09:25:06 +0000

    Up to 3.1

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,5 @@
-# Write your code here
+def cakes(eggs, butter, flour):
+    eggz = eggs // 5
+    butterz = butter // 250
+    flourz = flour // 250
+    return min(eggz, butterz, flourz)
--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,12 @@
-# Write your code here
+def last_digit(x):
+    return x % 10
+
+def remove_last_digit(x):
+    return x // 10
+
+def digit_sum(x):
+    tot = 0
+    while x > 0:
+        tot += last_digit(x)
+        x = remove_last_digit(x)
+    return tot
